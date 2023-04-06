32efe9d88da86eef9a5a8608930385419acb6d8a
Nazwan Hilmi <nhr.bzl@gmail.com>
2023-04-06 15:45:41 +0700

    update foto profile

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <div>
             <section className="jumbotron text-center">
-                <img src={profile} className='rounded-circle image-thumbnail' alt="photo-profile" width={300} height={300} />
+                <img src={profile} className='rounded-circle image-thumbnail' alt="photo-profile" width={200} />
                 <h1 className="display-4">Nazwan Hilmi</h1>
                 <p className="lead">High School Students</p>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L26.7,170.7C53.3,181,107,203,160,197.3C213.3,192,267,160,320,133.3C373.3,107,427,85,480,96C533.3,107,587,149,640,160C693.3,171,747,149,800,128C853.3,107,907,85,960,101.3C1013.3,117,1067,171,1120,186.7C1173.3,203,1227,181,1280,176C1333.3,171,1387,181,1413,186.7L1440,192L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
